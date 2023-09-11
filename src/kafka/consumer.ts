5ba67b9e3a3a85797a82d3f5e3a21202db343efb
Md Shafikur Rahaman <58303621+mdshofiur@users.noreply.github.com>
2023-09-11 20:54:49 +0600

    consumer function improve

--- src/kafka/consumer.ts
+++ src/kafka/consumer.ts
@@ -1,22 +1,27 @@
 import { kafka } from "../../config/kafka-config";
 
-export async function consumeFromKafka() {
-  const consumer = kafka.consumer({ groupId: "test-group" });
-  await consumer.connect();
-  await consumer.subscribe({ topic: "test-topic", fromBeginning: true });
 
-  const messages: any = []; // Store Kafka messages
-
-  await consumer.run({
-    eachMessage: async ({ topic, partition, message }: any) => {
-      console.log({
-        value: `message ${message.value}`,
-        
-      });
-
-      messages.push(`message ${message.value}`);
-    },
-  });
-  return messages;
- 
+export async function consumeFromKafka() { 
+    try {
+        const consumer = kafka.consumer({ groupId: "test-group" });
+        await consumer.connect();
+        await consumer.subscribe({ topic: "my-topic", fromBeginning: true });
+      
+        const messages: any[] = []; // Store Kafka messages
+      
+        await new Promise(async (resolve) => {
+          await consumer.run({
+            eachMessage: async ({ topic, partition, message }: any) => {
+              messages.push(message.value);
+            },
+          });
+          await consumer.disconnect();
+          resolve(messages);
+        });
+        console.log("messages", messages);
+        return messages;
+    } catch (error) {
+        console.log("error", error);
+    }
 }
+
